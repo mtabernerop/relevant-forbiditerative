de9632c5e7d3fc0dc4a280ef7f2fd12a138df686
migueltaber <100405958@alumnos.uc3m.es>
2022-03-30 14:32:56 +0200

    comment

--- src/search/plans/plans_graph.cc
+++ src/search/plans/plans_graph.cc
@@ -908,7 +908,7 @@ void PlansGraph::find_plans_dfs(size_t number_of_plans) {
             }
 
             // Filtering the current plan
-            string syscall = "python " + task_details[0] + "/filter_plans.py"
+            string syscall = "python " + task_details[0] + "/filter_plans.py" // change in clusters for /forbiditerative/filter_plans.py
                                                         " " + string(task_details[1]) +
                                                         " " + string(task_details[2]) +
                                                         " " + string(plan_dir) +
